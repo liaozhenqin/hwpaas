de310d15e42e8e07d578e35f24423215d9429a26
liaozhenqin <liaozhenqin@gmail.com>
2017-08-19 13:25:25 +0800

    Update CsedemoImpl.java

--- src/main/java/com/services/pojo/csedemo/CsedemoImpl.java
+++ src/main/java/com/services/pojo/csedemo/CsedemoImpl.java
@@ -33,9 +33,7 @@ public class CsedemoImpl implements Csedemo {
 
     public String sayHi(String name) {
         String str_PageHTML;
-        str_PageHTML = "<html><img src=\\"http://qcloud.dpfile.com/pc/h986KnOwBtGha2Lh8__w_YdJZzdUqlkBX_zrUQX7rpM9Vv-K7K1lRSAtc0Rwz-
-
-rWTYGVDmosZWTLal1WbWRW3A.jpg\\"></html>";
+        str_PageHTML = "<html><img src=\\"http://qcloud.dpfile.com/pc/h986KnOwBtGha2Lh8__w_YdJZzdUqlkBX_zrUQX7rpM9Vv-K7K1lRSAtc0Rwz-rWTYGVDmosZWTLal1WbWRW3A.jpg\\"></html>";
 
         return str_PageHTML;
     }
